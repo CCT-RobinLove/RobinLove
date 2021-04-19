3705856d8809d59af7d167660d915e0cf65364c1
SornrasakC <43643389+SornrasakC@users.noreply.github.com>
2021-04-19 16:40:00 +0700

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,23 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Home() {
-  return (
-    <div className="">
-      <Head>
-        <title>RobinLove</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      I am RobinLove I am loving you
-    </div>
-  )
+    const [test, setTest] = useState("default test");
+    useEffect(() => {
+        axios
+            .get("/test")
+            .then((r) => setTest(r.data))
+            .catch((e) => console.log(e, "reeeeeeee"));
+    }, []);
+    return (
+        <div className="">
+            <Head>
+                <title>RobinLove</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            {test}
+            I am RobinLove I am loving you
+        </div>
+    );
 }
