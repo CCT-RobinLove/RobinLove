ac9f5f4f46e006bf256e2e3b8c97dcddd4e82c48
Chomtana <Chomtana001@gmail.com>
2021-04-21 21:50:49 +0700

    Try merge

---
+++ pages/login.tsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function Login() {
+    const [test, setTest] = useState("default test");
+    useEffect(() => {
+        axios
+            .get("/test")
+            .then((r) => setTest(r.data))
+            .catch((e) => console.log(e, "reeeeeeee"));
+    }, []);
+    return (
+        <div className="">
+            <Head>
+                <title>RobinLove</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            {test}
+            I am RobinLove I am loving you
+        </div>
+    );
+}
