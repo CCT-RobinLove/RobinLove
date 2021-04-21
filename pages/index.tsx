c0e434671fe30aa364e6a34f6a7c3e5d2b34e2ff
SornrasakC <43643389+SornrasakC@users.noreply.github.com>
2021-04-21 20:46:43 +0700

    add auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,14 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 // import axios from "axios";
 import Amplify, { API } from "aws-amplify";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
-export default function Home() {
+function Home() {
     const [test, setTest] = useState("default test");
     useEffect(() => {
         console.log("reeee");
         API.get("test", "/test", {}).then((r) => {
-            console.log('asdasdasdasd');
+            console.log("asdasdasdasd");
             console.log(r);
         });
     }, []);
@@ -18,8 +19,11 @@ export default function Home() {
                 <title>RobinLove</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            <AmplifySignOut />
             <div>{test}</div>
             <div>I am RobinLove I am loving you</div>
         </div>
     );
 }
+
+export default withAuthenticator(Home);
