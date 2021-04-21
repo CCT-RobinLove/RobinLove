979db973a820f489d6a936675fca4926511d7457
SornrasakC <43643389+SornrasakC@users.noreply.github.com>
2021-04-21 20:28:25 +0700

    try aws-amplify

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
 import Head from "next/head";
 import { useEffect, useState } from "react";
-import axios from "axios";
+// import axios from "axios";
+import Amplify, { API } from "aws-amplify";
 
 export default function Home() {
     const [test, setTest] = useState("default test");
     useEffect(() => {
-        axios
-            .get("/test")
-            .then((r) => setTest(r.data))
-            .catch((e) => console.log(e, "reeeeeeee"));
+        console.log("reeee");
+        API.get("/test").then((r) => console.log(r));
+        // axios
+        //     .get("/test")
+        //     .then((r) => setTest(r.data))
+        //     .catch((e) => console.log(e, "reeeeeeee"));
     }, []);
     return (
         <div className="">
@@ -16,8 +19,8 @@ export default function Home() {
                 <title>RobinLove</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            {test}
-            I am RobinLove I am loving you
+            <div>{test}</div> 
+            <div>I am RobinLove I am loving you</div>
         </div>
     );
 }
