7277ef715c88a39b120c16e13c972695cbb4282a
Chomtana <Chomtana001@gmail.com>
2021-04-23 16:13:41 +0700

    Merge UI

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Header() {
+  return (
+    <div className="bg-primary w-full text-center text-4xl">
+      RobinLove
+    </div>
+  )
+}
---
+++ pages/addImage.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function addImage(props) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
