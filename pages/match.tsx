d196d762cd5fc9b9bc3daa799d84fa828bc750d9
Chomtana <Chomtana001@gmail.com>
2021-04-25 12:29:10 +0700

    Add matches page

--- pages/match.tsx
+++ pages/match.tsx
@@ -1,21 +1,35 @@
-import React from "react"
-import Header from "../components/Header"
-import ImageUploader from "../components/ImageUploader"
+import React from "react";
+import Header from "../components/Header";
+import ImageUploader from "../components/ImageUploader";
 
 export default function Match(props) {
   return (
     <div className="h-full">
       <Header></Header>
 
-      <div className="flex flex-col justify-center items-center w-full" id="avatar-uploader" style={{height: "calc(100% - 60px)"}}>
-        <div className="mb-3">
-          <ImageUploader></ImageUploader>
-        </div>
+      <div className="p-4">
+        <div className="text-3xl mb-4">Matches</div>
 
-        <button className="bg-primary rounded text-white text-center p-2 text-lg" style={{width: 240}}>
-          Continue
-        </button>
+        <div className="flex flex-col w-full">
+          <div
+            className="flex py-3 items-center"
+            style={{
+              borderBottom: "1px solid gray",
+            }}
+          >
+            <img
+              src="https://banner2.cleanpng.com/20180418/xqw/kisspng-avatar-computer-icons-business-business-woman-5ad736ba3f2735.7973320115240536902587.jpg"
+              style={{
+                width: 32,
+                height: 32,
+                objectFit: "cover",
+                marginRight: 16,
+              }}
+            ></img>
+            <div className="text-lg">Chomtana</div>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
