e7ac560773ec0541a7bbc84a784b8f659ebd642c
Chomtana <Chomtana001@gmail.com>
2021-04-26 23:08:50 +0700

    Add matched page

---
+++ pages/matched.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import Header from "../components/Header"
+
+export default function Matched(props) {
+  return (
+    <div className="h-full flex flex-col">
+      <Header></Header>
+
+      <div className="flex flex-col justify-around items-center flex-grow">
+        <div>
+          <img src={"https://banner2.cleanpng.com/20180418/xqw/kisspng-avatar-computer-icons-business-business-woman-5ad736ba3f2735.7973320115240536902587.jpg"} style={{
+            width: 240,
+            height: 240,
+            borderRadius: "50%",
+            objectFit: "cover",
+          }}></img>
+        </div>
+  
+        <div className="flex flex-row justify-around w-full pb-12">
+          <div className="text-4xl">Matched</div>
+        </div>
+      </div>
+    </div>
+  )
+}
