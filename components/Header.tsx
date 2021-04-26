63d6a585127d642244f3c550194d56d19ff1fd92
Chomtana <Chomtana001@gmail.com>
2021-04-26 09:46:32 +0700

    Update frontend

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,14 @@ import React from "react"
 
 export default function Header() {
   return (
-    <div className="bg-primary w-full text-center text-3xl text-white p-3">
-      RobinLove
+    <div className="flex flex-row items-center bg-primary w-full">
+      <div className="text-3xl text-white px-3" onClick={() => window.history.back()}>
+        ⇦
+      </div>
+
+      <div className="text-center text-3xl text-white p-3 pr-12 flex-grow">
+        RobinLove
+      </div>
     </div>
   )
 }
--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,13 +3,6 @@
 
 const awsmobile = {
     "aws_project_region": "ap-northeast-1",
-    "aws_cloud_logic_custom": [
-        {
-            "name": "uploadApi",
-            "endpoint": "https://cz9w4o8csg.execute-api.ap-northeast-1.amazonaws.com/devtokyo",
-            "region": "ap-northeast-1"
-        }
-    ],
     "aws_cognito_identity_pool_id": "ap-northeast-1:12ed0bb0-2d32-4b90-9d82-9eaab94c1852",
     "aws_cognito_region": "ap-northeast-1",
     "aws_user_pools_id": "ap-northeast-1_owRVrumkQ",
